2698de895ff4f549ffc57076cc5c88b0310087bb
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-02-04 09:12:29 +0100

    update: add fetch methode as asynchron methode with streaming data state

--- src/utiles/useFetch.js
+++ src/utiles/useFetch.js
@@ -1,13 +1,36 @@
 import { useState, useEffect } from "react";
 
 const useFetch = () => {
+
+    // Streaming Data state as Array
   const [streamingData, setstreamingData] = useState();
 
-  useEffect(() => {
-    setstreamingData("Hi Pockemon");
-  });
+ 
 
-  return streamingData;
+
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+    .then(async response => {
+        const data = await response.json();
+
+        // check for error response
+        if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+        setstreamingData(data)
+       
+    })
+    .catch(error => {
+       
+        console.error('There was an error!', error);
+       
+    });
+  },[]);
+
+  return [streamingData];
 };
 
 
