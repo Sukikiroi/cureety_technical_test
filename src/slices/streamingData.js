50d96f3d3d321c62bdb787696a6840c59dc916d9
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-02-04 08:30:02 +0100

    feature:add  addfetchedata action to the streaming data slice

--- src/slices/streamingData.js
+++ src/slices/streamingData.js
@@ -8,11 +8,13 @@ export const streamingdataSlice = createSlice({
   name: 'streamingdata',
   initialState,
   reducers: {
-  
+  addfetchedata:(state,action)=>{
+  state.streamingdata.push(action.payload)
+  }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const {  } = streamingdataSlice.actions
+export const { addfetchedata } = streamingdataSlice.actions
 
 export default streamingdataSlice.reducer
