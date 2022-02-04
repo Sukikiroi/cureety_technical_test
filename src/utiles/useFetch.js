4a343273bf364e36c4133fb083756f53adebbc09
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-02-04 09:16:45 +0100

    update: add Error  state as boolean

--- src/utiles/useFetch.js
+++ src/utiles/useFetch.js
@@ -1,42 +1,37 @@
 import { useState, useEffect } from "react";
 
 const useFetch = () => {
-
-    // Streaming Data state as Array
+  // Streaming Data state as Array
   const [streamingData, setstreamingData] = useState();
 
-   // Loading State  as Boolean
-   const [loading, setloading] = useState(true);
+  // Loading State  as Boolean
+  const [loading, setloading] = useState(true);
 
- 
-
-
+  // error State as Boolean
+  const [error, seterror] = useState(false);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur')
-    .then(async response => {
+    fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur")
+      .then(async (response) => {
         const data = await response.json();
 
         // check for error response
         if (!response.ok) {
-            // get error message from body or default to response statusText
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
+          // get error message from body or default to response statusText
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
         }
-        setstreamingData(data)
-        setloading(false)
-       
-    })
-    .catch(error => {
-       
-        console.error('There was an error!', error);
-       
-    });
-  },[]);
-    //  return states as Array
-  return [streamingData,loading];
+        setstreamingData(data);
+        setloading(false);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+        seterror(true)
+      });
+  }, []);
+  //  return states as Array
+  return [streamingData, error, loading];
 };
 
-
 // Exporting the hook
-export default useFetch
+export default useFetch;
