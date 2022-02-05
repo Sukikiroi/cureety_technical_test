8229e15d475aebaa90d4823ccf6e9d68f6df457c
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-02-05 16:50:49 +0100

    feature: add Data Displayer Component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import Pokemone from "./components/pokemone";
 
 import MyFancyLoader from "./components/myfancyLoader";
 
+import PokemonContainer from "./components/pokemonContainer"
+
 function App() {
   const [data, error, loading] = useFetch("wormadam");
   if (!loading) {
     console.log(data);
   }
   //  console.log(JSON.parse(localStorage.getItem("data")))
-  return <div className="App">{loading ? <MyFancyLoader /> : <></>}</div>;
+  return (
+    <div className="App">
+      {loading ? <MyFancyLoader /> : <PokemonContainer/>}
+    </div>
+  );
 }
 
 export default App;
