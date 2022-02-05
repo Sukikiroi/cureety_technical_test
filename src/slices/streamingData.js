1a665a4e3ede463e352bee7f15386acb448436e1
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-02-05 18:37:34 +0100

    feature: add update DSL Configuration Action to redux state

--- src/slices/streamingData.js
+++ src/slices/streamingData.js
@@ -1,33 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 // Localstorage package
-import {saveState} from "../utiles/localstorage"
+import { saveState } from "../utiles/localstorage";
 
 // Envirenemnt Global Varibale
-import {BaseUrl,authentificationHeader,compressionTimeoutDelay,maximumSize,protectedFromCleaning} from "../utiles/global"
+import {
+  BaseUrl,
+  authentificationHeader,
+  compressionTimeoutDelay,
+  maximumSize,
+  protectedFromCleaning,
+} from "../utiles/global";
 
 const initialState = {
-  streamingdata: []  ,
-  BaseUrl:BaseUrl,
-  authentificationHeader:authentificationHeader,
-  compressionTimeoutDelay:compressionTimeoutDelay,
-  maximumSize:maximumSize,
-  protectedFromCleaning:protectedFromCleaning,
-
+  streamingdata: [],
+  BaseUrl: BaseUrl,
+  authentificationHeader: authentificationHeader,
+  compressionTimeoutDelay: compressionTimeoutDelay,
+  maximumSize: maximumSize,
+  protectedFromCleaning: protectedFromCleaning,
 };
-console.log(initialState)
+console.log(initialState);
 export const streamingdataSlice = createSlice({
-  name: 'streamingdata',
+  name: "streamingdata",
   initialState,
   reducers: {
-  addfetchedata:(state,action)=>{
-  state.streamingdata.push(action.payload[0])
-  saveState(action.payload[0].key,action.payload[0])
-  }
+    addfetchedata: (state, action) => {
+      state.streamingdata.push(action.payload[0]);
+      saveState(action.payload[0].key, action.payload[0]);
+    },
+    updateDSLConfiguration: (state, action) => {
+      (state.BaseUrl = ""),
+        (state.authentificationHeader = ""),
+        (state.compressionTimeoutDelay = ""),
+        (state.maximumSize = ""),
+        (state.protectedFromCleaning = "");
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { addfetchedata } = streamingdataSlice.actions
+export const { addfetchedata, updateDSLConfiguration } =
+  streamingdataSlice.actions;
 
-export default streamingdataSlice.reducer
+export default streamingdataSlice.reducer;
