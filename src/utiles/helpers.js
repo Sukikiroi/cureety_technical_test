48f5fb69208163edff0766fe32e1e892ce575fec
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-02-05 18:30:51 +0100

    update: make Global Envirenement variabls to Global Redux State

--- src/utiles/helpers.js
+++ src/utiles/helpers.js
@@ -1,139 +1,126 @@
-
-var lzwCompress =require('lzwcompress') 
-var compressionTimeoutDelay=require('./global')
-
- 
-
- 
-
+var lzwCompress = require("lzwcompress");
 
 let data = [
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-    {
-      name: "John",
-      age: 30,
-      cars: ["Ford", "BMW", "Fiat"],
-    },
-  ];
- 
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+  {
+    name: "John",
+    age: 30,
+    cars: ["Ford", "BMW", "Fiat"],
+  },
+];
 
-
-  const  executionTimeCalculator = (value) => {
- var start = Date.now();
+const executionTimeCalculator = (value, compressionTimeoutDelay) => {
+  var start = Date.now();
 
   // task starts
-   
+
   const compressed = lzwCompress.pack(value);
   // task ends
 
   var end = Date.now();
   console.log(`Execution time: ${end - start} ms`);
-  let ExcutionTime=end - start
+  let ExcutionTime = end - start;
 
   //When compression takes more than compressionTimeoutDelay
-  if(ExcutionTime>compressionTimeoutDelay)
-        // return the uncompressed data
-      return value
-    //  if compression is fast  return the Copressed value
-  else
-      return compressed
+  if (ExcutionTime > compressionTimeoutDelay)
+    // return the uncompressed data
+    return value;
+  //  if compression is fast  return the Copressed value
+  else return compressed;
 };
 
-executionTimeCalculator(data)
-
- 
- 
+executionTimeCalculator(data);
